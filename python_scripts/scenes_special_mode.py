c2ae5e53438f40adf9c77b173710f15e0280ad2b
rapedapeda <takkie+github@muizeluuk.nl>
2020-04-24 16:45:33 +0200

    Print test outputs

--- python_scripts/scenes_special_mode.py
+++ python_scripts/scenes_special_mode.py
@@ -5,12 +5,16 @@
 #
 
 entity_id = data.get('entity_id')
-aantal = len(hass.states.get(entity_id).attributes['kamers'])
 kamers = hass.states.get(entity_id).attributes['kamers']
+print(kamers)
 scenes = hass.states.get(entity_id).attributes['scenes']
+print(scenes)
+aantal = len(hass.states.get(entity_id).attributes['kamers'])
+print(aantal)
+
 
 for i in range(aantal):
     entity = f'kamer_scenes_{kamers[i]}'
     option = scenes[i]
-    service_data = {"entity_id": entity, "option": option}
+    service_data = {"entity_id": entity_id, "option": option}
     hass.services.call("input_select", "select_option", service_data, False)
