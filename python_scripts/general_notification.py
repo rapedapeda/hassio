875c37bea7d84ea0136f8804a509e52ac97db5ed
rapedapeda <takkie+github@muizeluuk.nl>
2020-08-14 18:16:37 +0200

    First test for general messaging script

---
+++ python_scripts/general_notification.py
@@ -0,0 +1,18 @@
+#
+# Wordt gebruikt voor algemene notifications
+# Stuur de volgende attributen mee:
+# recipients (lijst), title, message, thread-id
+# Werking: checkt de lijst van recipients
+# Itereert en stuurt iedereen het bericht
+#
+recipients = data.get('recipients').split(',')
+title = data.get('title')
+message = data.get('message')
+threadid = data.get('thread-id')
+
+aantal = len(recipients)
+logger.warning("Recipients  {}".format(recipients))
+logger.warning("Title  {}".format(title))
+logger.warning("Message  {}".format(message))
+logger.warning("Thread-id  {}".format(threadid))
+
