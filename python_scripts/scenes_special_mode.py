ce728b8d6c9ca8cb23e5069a98ebcde867e8702c
Takkie <takkie+fork@muizeluuk.nl>
2021-02-08 23:08:42 +0100

    Prioriteringen toegevoegd + mode per kamer

--- python_scripts/scenes_special_mode.py
+++ python_scripts/scenes_special_mode.py
@@ -3,15 +3,15 @@
 # Loopt over de attributen (kamers,scenes)
 # Itereert en zet alle kamers op de goede scene
 #
-kamers = data.get('kamers').split(',')
-scenes = data.get('scenes').split(',')
-aantal = len(kamers)
+kamer = data.get('kamer').split(',')
+scene = data.get('scene').split(',')
+aantal = len(kamer)
 #logger.warning("kamers  {}".format(kamers))
 #logger.warning("Scenes  {}".format(scenes))
 #logger.warning("Aantal  {}".format(aantal))
 
 for i in range(aantal):
-    entity = f'input_select.kamer_scenes_modus_{kamers[i]}'
-    option = scenes[i]
+    entity = f'input_select.kamer_scenes_modus_{kamer[i]}'
+    option = scene[i]
     service_data = {"entity_id": entity, "option": option}
     hass.services.call("input_select", "select_option", service_data, False)
