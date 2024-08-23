0a31518fc49ae645923bfc0b477280f56a594e2d
rapedapeda <takkie+github@muizeluuk.nl>
2024-08-24 01:11:56 +0200

    Automatisch ophalen van trackers

--- appdaemon/apps/alarm/alarm_mode.py
+++ appdaemon/apps/alarm/alarm_mode.py
@@ -5,17 +5,22 @@ class AlarmMode(hass.Hass):
     AppDaemon-app voor het beheren van de alarmstatus op basis van aanwezigheid en tijd van de dag.
     """
 
-    def initialize(self):
+     def initialize(self):
         """
         Initialiseert de app door:
-        - Luisteren naar veranderingen in de aanwezigheidstoestand
+        - Luisteren naar veranderingen in de aanwezigheidstoestand van alle gedefinieerde personen
         - Planning van callbacks voor zonsondergang en zonsopgang met offsets
         - Voer een initiële controle van de alarmstatus uit
         """
-        self.log("Hello from AlarmMode")
-        # Luister naar veranderingen in de aanwezigheidstoestand
-        self.listen_state(self.handle_alarm, self.anyone_home(person=True))
-
+        
+        # Verkrijg alle device_tracker entiteiten
+        self.trackers = self.get_trackers()
+        self.log(f'Trackers: {self.trackers}')
+        
+        # Dynamisch luisteren naar de aanwezigheidstoestand van elke tracker
+        for tracker in self.trackers:
+            self.listen_state(self.handle_alarm, tracker)
+        
         # Plan callbacks voor zonsondergang en zonsopgang
         self.run_at_sunrise(self.handle_alarm, offset=3600)
         self.run_at_sunset(self.handle_alarm, offset=-3600)  # 15 minuten voor zonsondergang
@@ -27,12 +32,11 @@ class AlarmMode(hass.Hass):
         """
         Behandelt het instellen van de alarmstatus op basis van aanwezigheid en tijd van de dag.
         """
-        presence_status = self.get_state("input_boolean.presence_status")
         night = self.now_is_between("sunset - 01:00:00", "sunrise + 01:00:00")
 
-        if presence_status == 'off':
+        if self.noone_home():
             self.call_service("alarm_control_panel/alarm_arm_away", entity_id="alarm_control_panel.huis", code=0000)
-        elif presence_status == 'on':
+        elif self.anyone_home():
             if night:
                 self.call_service("alarm_control_panel/alarm_arm_home", entity_id="alarm_control_panel.huis", code=0000)
             else:
