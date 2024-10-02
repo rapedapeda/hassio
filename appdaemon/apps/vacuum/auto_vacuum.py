deedc15658fac111ebe540008794054ea20e0670
rapedapeda <takkie+github@muizeluuk.nl>
2024-10-02 18:27:15 +0200

    Typos

--- appdaemon/apps/vacuum/auto_vacuum.py
+++ appdaemon/apps/vacuum/auto_vacuum.py
@@ -1,7 +1,7 @@
 import appdaemon.plugins.hass.hassapi as hass
 from datetime import datetime, timedelta
 
-class AutoVacuum():
+class AutoVacuum(hass.Hass):
     
     def initialize(self):
         self.last_cleaned = {}
